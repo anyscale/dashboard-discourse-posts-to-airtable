baad0813024df66b03f7bf00159c780423c3b7fc
Huaiwei Sun <scottsun94@gmail.com>
2022-09-30 15:56:42 -0700

    modified the script to call discourse api
    Signed-off-by: Huaiwei Sun <scottsun94@gmail.com>

--- index.js
+++ index.js
@@ -11,59 +11,41 @@ const octokit = new (Octokit.plugin(paginateRest))({
 });
 
 async function main() {
-  const issueNumberToRecord = {};
+  const topicIDToRecord = {};
   await base
-    .select({ view: "Default", fields: ["Number"] })
+    .select({ view: "Default", fields: ["Link"] })
     .eachPage((records, fetchNextPage) => {
       records.forEach((record) => {
-        issueNumberToRecord[record.get("Number")] = record.getId();
+        topicIDToRecord[record.get("Link")] = record.getId();
       });
       fetchNextPage();
     });
   console.log(
-    `Fetched ${Object.keys(issueNumberToRecord).length} records from airtable.`
+    `Fetched ${Object.keys(topicIDToRecord).length} records from airtable.`
   );
 
-  const githubIssues = {};
-  let dateAfter = new Date();
-  dateAfter.setDate(dateAfter.getDate() - 14);
-  for await (const response of octokit.paginate.iterator(
-    "GET /repos/{owner}/{repo}/issues",
-    {
-      owner: "ray-project",
-      repo: "ray",
-      since: dateAfter.toISOString(),
-      labels: ["dashboard"],
-      per_page: 100,
-      state: "all",
-      pull_request: false,
-    }
-  )) {
-    for (const issue of response.data) {
-      githubIssues[issue.number.toString()] = {
+  const discourseTopics = {};
+  for await (const response = fetch('https://discuss.ray.io/c/dashboard/9.json')
+  ) {
+    for (const topic of response.data.topic_list.topics) {
+      discourseTopics[topic.id.toString()] = {
         fields: {
-          Number: issue.number,
-          Title: issue.title,
-          Labels: issue.labels.map((label) => label.name),
-          Milestone: issue.milestone?.title,
-          Link: issue.html_url,
-          CreatedAt: issue.created_at,
-          UpdatedAt: issue.updated_at,
-          State: issue.state,
-          Priority: issue.labels.filter((label) =>
-            label.name.startsWith("P")
-          )[0]?.name,
+          URL: "https://discuss.ray.io/t"+"/"+topic.slug+"/"+topic.id,
+          ID: topic.id,
+          Title: topic.title,
+          CreatedAt: topic.created_at,
+          UpdatedAt: topic.last_posted_at,
         },
       };
     }
   }
-  console.log(`Fetched ${Object.keys(githubIssues).length} issues from github`);
+  console.log(`Fetched ${Object.keys(discourseTopics).length} topics from Discourse`);
 
-  const airTableNumbers = new Set(Object.keys(issueNumberToRecord));
-  const recordToAdd = Object.entries(githubIssues)
+  const airTableNumbers = new Set(Object.keys(topicIDToRecord));
+  const recordToAdd = Object.entries(discourseTopics)
     .filter(([number, _]) => !airTableNumbers.has(number))
     .map(([_, record]) => record);
-  const recordToUpdate = Object.entries(githubIssues)
+  const recordToUpdate = Object.entries(discourseTopics)
     .filter(([number, _]) => airTableNumbers.has(number))
     .map(([_, record]) => record);
 
@@ -78,7 +60,7 @@ async function main() {
   console.log(`Updating ${recordToUpdate.length} records`);
   for (let i = 0; i < recordToUpdate.length; i += 10) {
     const chunk = recordToUpdate.slice(i, i + 10).map((record) => ({
-      id: issueNumberToRecord[record.fields["Number"].toString()],
+      id: topicIDToRecord[record.fields["URL"].toString()],
       fields: record.fields,
     }));
     await base.replace(chunk, {
